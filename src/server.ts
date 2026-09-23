d5648c6bb06dcbdbd198d6858fb1698d0cef141c
soliboyev muhammadyusuf <soliboyev007@soliboyevs-MacBook-Air.local>
2026-09-23 18:59:37 +0900

    feat: express integrations

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import mongoose from "mongoose";
+import app from "./app";
 
 mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then(() => {
     console.log("Connected to MongoDB");
     const PORT = process.env.PORT || 3003;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
   })
   .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
