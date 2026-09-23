72d7ae7b773ba7030d9f4bba22f85cd1bf45bf44
soliboyev muhammadyusuf <soliboyev007@soliboyevs-MacBook-Air.local>
2026-09-23 19:05:41 +0900

    feat: m task is done

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,8 @@
-console.log("heello world");
+function getSquareNumbers(numbers: number[]) {
+  return numbers.map((number) => ({
+    number: number,
+    square: number ** 2,
+  }));
+}
+
+console.log(getSquareNumbers([1, 2, 3]));
